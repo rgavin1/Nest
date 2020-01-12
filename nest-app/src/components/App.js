6bcd9eca36abdd0fe80125d20810460fae7a1c58
rgavin1 <44215916+rgavin1@users.noreply.github.com>
2020-01-12 10:16:02 -0500

    Fixes to Navigation spacing

--- nest-app/src/components/App.js
+++ nest-app/src/components/App.js
@@ -2,21 +2,28 @@ import React from 'react';
 import '../styles/App.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import Hero from '../assets/Hero/Farmhouses-479.png';
 
 function App() {
   return (
     <div className="App">
-      <div className="brand-container">
-        <span className="brand">N</span>
-        <div>NEST</div>
-      </div>
-      <div className="hamburger-container">
-        <div className="menu">
-          <FontAwesomeIcon icon={faBars} ></FontAwesomeIcon>
+      <nav>
+        <div className="brand-container">
+          <span className="brand">N</span>
+          <div>NEST</div>
         </div>
-      </div>
+        <div className="hamburger-container">
+          <div className="menu">
+            <FontAwesomeIcon icon={faBars} ></FontAwesomeIcon>
+          </div>
+        </div>
+      </nav>
       <div className="hero-container">
+        <div className="hero-image"></div>
         <div className="welcome">Welcome Home</div>
+      </div>
+      <div>
+        11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
       </div>
     </div>
   );
