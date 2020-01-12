32bd24ad52c101f6a6f8e16bf79758a0d7de3fbd
rgavin1 <44215916+rgavin1@users.noreply.github.com>
2020-01-12 11:06:55 -0500

    Deleted all progress, couldn't figure out why the sticky top nav wasnt working

--- nest-app/src/components/App.js
+++ nest-app/src/components/App.js
@@ -1,30 +1,24 @@
 import React from 'react';
 import '../styles/App.scss';
+import Navigation from './Nav';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import Hero from '../assets/Hero/Farmhouses-479.png';
+import { Controller, Scene } from 'react-scrollmagic';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <nav>
-        <div className="brand-container">
-          <span className="brand">N</span>
-          <div>NEST</div>
-        </div>
-        <div className="hamburger-container">
-          <div className="menu">
-            <FontAwesomeIcon icon={faBars} ></FontAwesomeIcon>
-          </div>
-        </div>
-      </nav>
-      <div className="hero-container">
-        <div className="hero-image"></div>
-        <div className="welcome">Welcome Home</div>
-      </div>
-      <div>
-        11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
-      </div>
+      <Navigation />
+      <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non arcu risus quis varius quam quisque id. Ut eu sem integer vitae justo. Egestas erat imperdiet sed euismod nisi porta lorem mollis aliquam. Condimentum lacinia quis vel eros donec ac odio. Ipsum consequat nisl vel pretium lectus quam id leo. In cursus turpis massa tincidunt. Aliquam faucibus purus in massa. Volutpat est velit egestas dui. Neque egestas congue quisque egestas diam in arcu cursus euismod.
+
+Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Amet nisl purus in mollis nunc sed id. Mattis enim ut tellus elementum sagittis vitae et. Tincidunt eget nullam non nisi est sit amet. Metus dictum at tempor commodo ullamcorper a lacus vestibulum sed. Amet consectetur adipiscing elit ut aliquam purus sit amet. Mauris nunc congue nisi vitae suscipit tellus mauris. Pulvinar pellentesque habitant morbi tristique. Pharetra et ultrices neque ornare aenean. Montes nascetur ridiculus mus mauris vitae ultricies leo integer malesuada. Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet.
+
+Adipiscing commodo elit at imperdiet dui accumsan sit. Sapien faucibus et molestie ac feugiat. Nec sagittis aliquam malesuada bibendum arcu. Ullamcorper eget nulla facilisi etiam dignissim diam quis. Semper viverra nam libero justo laoreet sit. Vel facilisis volutpat est velit egestas dui id ornare. Aliquam purus sit amet luctus venenatis lectus. Velit ut tortor pretium viverra. Eget mauris pharetra et ultrices neque. Dui vivamus arcu felis bibendum ut tristique.
+
+Eu ultrices vitae auctor eu augue ut lectus arcu. In fermentum et sollicitudin ac orci phasellus egestas tellus. Cursus metus aliquam eleifend mi in nulla posuere sollicitudin aliquam. Vitae auctor eu augue ut lectus. Aenean sed adipiscing diam donec. Nullam vehicula ipsum a arcu cursus vitae congue mauris. Ac ut consequat semper viverra nam libero justo. Nunc sed augue lacus viverra vitae congue eu. Facilisis mauris sit amet massa vitae tortor. Faucibus a pellentesque sit amet porttitor. Cursus metus aliquam eleifend mi in. Id ornare arcu odio ut sem. Urna nunc id cursus metus aliquam eleifend. Neque laoreet suspendisse interdum consectetur libero.
+
+Sit amet aliquam id diam maecenas ultricies mi. Ut eu sem integer vitae justo eget. Sodales ut etiam sit amet nisl purus in. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Habitant morbi tristique senectus et. Pharetra massa massa ultricies mi quis hendrerit. Ultrices neque ornare aenean euismod elementum nisi quis. Mauris rhoncus aenean vel elit scelerisque. Enim tortor at auctor urna nunc id. Sit amet nisl purus in mollis nunc sed id semper. Ullamcorper eget nulla facilisi etiam dignissim diam. Molestie ac feugiat sed lectus.</div>
     </div>
   );
 }
---
+++ nest-app/src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div className="nav-container">
+            <div className="brand-container">
+                <div>N</div>
+                <div>NEST</div>
+            </div>
+            <div>MENU</div>
+        </div>
+    )
+}
+
+export default Nav;
