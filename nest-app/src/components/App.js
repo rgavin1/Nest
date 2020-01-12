d852c97e94521f03550213c8964d5c1b6dc2c089
rgavin1 <44215916+rgavin1@users.noreply.github.com>
2020-01-12 03:28:00 -0500

    Hero Images Added

--- nest-app/src/components/App.js
+++ nest-app/src/components/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import '../styles/App.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
     <div className="App">
+      <div className="brand-container">
+        <span className="brand">N</span>
+        <div>NEST</div>
+      </div>
       <div className="hamburger-container">
         <div className="menu">
-          <div></div>
-          <div></div>
-          <div></div>
+          <FontAwesomeIcon icon={faBars} ></FontAwesomeIcon>
         </div>
+      </div>
+      <div className="hero-container">
+        <div className="welcome">Welcome Home</div>
       </div>
     </div>
   );
