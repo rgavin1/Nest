cc84edc3697a1a5f4b9ea6195a5feef33cd53193
rgavin1 <44215916+rgavin1@users.noreply.github.com>
2020-01-13 00:01:14 -0500

    Completed Mobile Menu

--- nest-app/src/components/Nav.js
+++ nest-app/src/components/Nav.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStream } from '@fortawesome/free-solid-svg-icons';
+import { faStream, faTimes } from '@fortawesome/free-solid-svg-icons';
 
-const Nav = () => {
+class Nav extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            toggled: true
+        }
+        this.handleToggle = this.handleToggle.bind(this);
+    }
+
+    handleToggle = () => {
+        this.setState({ toggled: !this.state.toggled }); 
+    }
+
+    render(){
     return (
         <div className="nav-container">
+            <div className={this.state.toggled ? '' : "nav-container dropdown-menu"}>
+                <ul>
+                    <li>Search</li>
+                    <li>Contact</li>
+                    <li>Login</li>
+                </ul>
+            </div>
             <div className="brand-container">
                 <div className="logo">N</div>
                 <div>NEST</div>
             </div>
-            <div className="menu-container">
-                <FontAwesomeIcon icon={faStream} />
+            <div onClick={this.handleToggle} className="menu-container">
+                { this.state.toggled ? <FontAwesomeIcon icon={faStream} /> : <FontAwesomeIcon icon={faTimes} /> }
             </div>
         </div>
-    )
+        )
+    }
 }
 
 export default Nav;
